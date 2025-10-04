00041183642ffb2808429eaa46fa3d4adfa3437d
ADITYA MAGDUM <magdumaditya2293@gmail.com>
2025-10-04 15:51:54 +0530

    Add files via upload

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,5 @@
+let name="Aditya"
+let pass=123
+let age=22
+console.log(typeof(name));
+console.log(typeof(age));
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const Name = "Aditya";
+let name="Shriyani";
+const Email="aditya@gmail.com";
+console.table([Name,Email]);
+console.log(name);
