081842c870d56a792dbde4990786563e538d5d6a
ADITYA MAGDUM <magdumaditya2293@gmail.com>
2025-10-09 07:47:04 +0530

    Add files via upload

---
+++ 01_basics/03_conversionDataTypes.js
@@ -0,0 +1,22 @@
+// let score = 33
+// let name = "Aditya"
+// console.log(typeof(score));
+// console.log(typeof(name));
+
+// let str= String(score);
+// let num = Number(name);
+// console.log(typeof(str));
+// console.log(typeof(num));
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1=100;
+++gameCounter1;
+console.log(gameCounter1);
+
+let gameCounter2=100;
+--gameCounter2;
+console.log(gameCounter2);
+
---
+++ 01_basics/04_math.js
@@ -0,0 +1,4 @@
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.round(4.6));
+console.log(Math.PI);
+console.log(Math.max(1,2,3,4));
