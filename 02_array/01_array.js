213fd70e46391f2b56135e2c56ee5c787acab7be
ADITYA MAGDUM <magdumaditya2293@gmail.com>
2025-10-09 08:03:26 +0530

    Add files via upload

---
+++ 02_array/01_array.js
@@ -0,0 +1,17 @@
+const myArr = ([1,2,3,4,5,6]);
+console.log("A",myArr);
+console.log(myArr.push(7));
+console.log(myArr);
+console.log(myArr.pop());
+console.log(myArr);
+
+let arr2= new Array(4,5,6,8,9);
+console.log("B",arr2);
+console.log(arr2.unshift(3));
+console.log(arr2.shift());
+console.log(arr2);
+
+console.log(arr2.slice(1,3));
+console.log(arr2.splice(0,3,1));
+
+
